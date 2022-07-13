169d93b26d3c5fa1046b722864f39c0fb83b1995
Jordan Wong <pjwong97@gmail.com>
2022-07-13 14:53:37 -0700

    makeBoard in progress

--- connect4.js
+++ connect4.js
@@ -7,18 +7,26 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
-function makeBoard() {
+function makeBoard(height = HEIGHT, width = WIDTH) {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  const rows = new Array(height);
+  for(let i = 0; i<height.length; i++){
+    board.push(rows[i] = new Array (width));
+    for(let j = 0; j<rows[i].length; j++){
+      board[i][j] = null;
+    }
+  }
+  console.log(board);
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
