39d340cbc1fb1828039eb99c92495045452bd6a3
Marshall Buck <dev@marshallbuck.com>
2022-07-13 17:13:05 -0500

    makeHTMLboard works as intended

--- connect4.js
+++ connect4.js
@@ -28,17 +28,20 @@ function makeBoard(height = HEIGHT, width = WIDTH) {
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
-function makeHtmlBoard() {
+function makeHtmlBoard(width = WIDTH, height = HEIGHT) {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  const htmlBoard = document.getElementById('board');
 
   // TODO: add comment for this code
-  var top = document.createElement("tr");
+  // Create table row element, set id to column-top, and add event listener
+  const top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
   // TODO: add comment for this code
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
+  // set each headCell to id of x, and append headCell to top, then append top to htmlBoard
+  for (let x = 0; x < width; x++) {
+    const headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
@@ -47,19 +50,20 @@ function makeHtmlBoard() {
   // dynamically creates the main part of html board
   // uses HEIGHT to create table rows
   // uses WIDTH to create table cells for each row
-  for (var y = 0; y < HEIGHT; y++) {
+  for (let y = 0; y < height; y++) {
     // TODO: Create a table row element and assign to a "row" variable
-
-    for (var x = 0; x < WIDTH; x++) {
+    const row = document.createElement('tr');
+    for (let x = 0; x < width; x++) {
       // TODO: Create a table cell element and assign to a "cell" variable
-
+      const cell = document.createElement('td');
       // TODO: add an id, y-x, to the above table cell element
       // you'll use this later, so make sure you use y-x
-
+      cell.id = `${y}-${x}`;
       // TODO: append the table cell to the table row
-
+      row.append(cell);
     }
     // TODO: append the row to the html board
+    htmlBoard.append(row);
 
   }
 }
